1e9da1b7637dff7e77eddab98edc408268b2d8f3
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-04-20 21:46:44 +0300

    show weak warnings with a proper highlight

--- crates/aptos-analyzer/src/lsp/to_proto.rs
+++ crates/aptos-analyzer/src/lsp/to_proto.rs
@@ -73,7 +73,7 @@ pub(crate) fn diagnostic_severity(severity: Severity) -> lsp_types::DiagnosticSe
     match severity {
         Severity::Error => lsp_types::DiagnosticSeverity::ERROR,
         Severity::Warning => lsp_types::DiagnosticSeverity::WARNING,
-        Severity::WeakWarning => lsp_types::DiagnosticSeverity::HINT,
+        Severity::WeakWarning => lsp_types::DiagnosticSeverity::INFORMATION,
         // unreachable
         Severity::Allow => lsp_types::DiagnosticSeverity::INFORMATION,
     }
