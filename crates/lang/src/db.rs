6e90d426673b9588ee345ac4644076639d7010a4
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-05-01 01:25:31 +0300

    optimize module resolution a bit

--- crates/lang/src/db.rs
+++ crates/lang/src/db.rs
@@ -1,21 +1,34 @@
 use crate::loc::{SyntaxLoc, SyntaxLocFileExt};
+use crate::nameres::address::Address;
 use crate::nameres::path_resolution;
-use crate::nameres::scope::{ScopeEntry, VecExt};
+use crate::nameres::scope::{NamedItemsExt, ScopeEntry, VecExt};
+use crate::node_ext::ModuleLangExt;
 use crate::types::inference::InferenceCtx;
 use crate::types::inference::ast_walker::TypeAstWalker;
 use crate::types::inference::inference_result::InferenceResult;
 use crate::types::ty::Ty;
+use base_db::package_root::PackageRootId;
 use base_db::{SourceDatabase, Upcast};
+use std::fs::File;
 use std::sync::Arc;
+use syntax::ast::Module;
 use syntax::ast::node_ext::move_syntax_node::MoveSyntaxNodeExt;
 use syntax::ast::node_ext::syntax_node::SyntaxNodeExt;
-use syntax::files::{InFile, InFileExt};
+use syntax::files::{InFile, InFileExt, InFileVecExt};
 use syntax::{AstNode, ast};
+use vfs::FileId;
 
 #[ra_salsa::query_group(HirDatabaseStorage)]
 pub trait HirDatabase: SourceDatabase + Upcast<dyn SourceDatabase> {
     fn resolve_path(&self, path_loc: SyntaxLoc) -> Vec<ScopeEntry>;
+
     fn inference_for_ctx_owner(&self, ctx_owner_loc: SyntaxLoc, msl: bool) -> Arc<InferenceResult>;
+
+    fn file_ids_by_module_address(
+        &self,
+        package_root_id: PackageRootId,
+        address: Address,
+    ) -> Vec<FileId>;
 }
 
 pub(crate) fn resolve_path(db: &dyn HirDatabase, path_loc: SyntaxLoc) -> Vec<ScopeEntry> {
@@ -70,4 +83,30 @@ impl<T: AstNode> NodeInferenceExt for InFile<T> {
         let inference = db.inference_for_ctx_owner(inference_owner.in_file(file_id).loc(), msl);
         Some(inference)
     }
+}
+
+fn file_ids_by_module_address(
+    db: &dyn HirDatabase,
+    package_root_id: PackageRootId,
+    address: Address,
+) -> Vec<FileId> {
+    let source_file_ids = db.source_file_ids(package_root_id);
+
+    let mut file_ids = vec![];
+    for source_file_id in source_file_ids {
+        let modules = get_modules_in_file(db, source_file_id, address.clone());
+        if !modules.is_empty() {
+            file_ids.push(source_file_id);
+        }
+    }
+    file_ids
+}
+
+pub(crate) fn get_modules_in_file(db: &dyn HirDatabase, file_id: FileId, address: Address) -> Vec<Module> {
+    let source_file = db.parse(file_id).tree();
+    let modules = source_file
+        .all_modules()
+        .filter(|m| m.address_equals_to(address.clone(), false))
+        .collect::<Vec<_>>();
+    modules
 }
