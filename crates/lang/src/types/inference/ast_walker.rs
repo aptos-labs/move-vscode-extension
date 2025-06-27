e2923cb056ef0d061a1f06d9cdb2153daf000671
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-06-27 19:48:27 +0300

    ignore lambda return type inference test for now

--- crates/lang/src/types/inference/ast_walker.rs
+++ crates/lang/src/types/inference/ast_walker.rs
@@ -1,4 +1,5 @@
 mod infer_specs;
+mod lambda_expr;
 
 use crate::nameres::name_resolution::get_entries_from_walking_scopes;
 use crate::nameres::namespaces::NAMES;
@@ -756,39 +757,6 @@ impl<'a, 'db> TypeAstWalker<'a, 'db> {
         }
 
         Ty::Unknown
-    }
-
-    fn infer_lambda_expr(&mut self, lambda_expr: &ast::LambdaExpr, expected: Expected) -> Ty {
-        let mut param_tys = vec![];
-
-        for (lambda_param, ident_pat) in lambda_expr.params_with_ident_pats() {
-            let file_id = self.ctx.file_id;
-            let param_ty = match lambda_param.type_() {
-                Some(type_) => self.ctx.ty_lowering().lower_type(type_.in_file(file_id)),
-                None => Ty::new_ty_var(&self.ctx.ty_var_index),
-            };
-            self.ctx.pat_types.insert(ident_pat.into(), param_ty.clone());
-            param_tys.push(param_ty);
-        }
-
-        let lambda_call_ty = TyCallable::new(
-            param_tys,
-            Ty::new_ty_var(&self.ctx.ty_var_index),
-            CallKind::Lambda,
-        );
-        self.ctx.lambda_exprs.push(lambda_expr.clone());
-        self.ctx
-            .lambda_expr_types
-            .insert(lambda_expr.clone(), lambda_call_ty.clone());
-
-        let lambda_ty = Ty::Callable(lambda_call_ty);
-        if let Some(expected_ty) = expected.ty(self.ctx) {
-            // error if not TyCallable
-            self.ctx
-                .coerce_types(lambda_expr.node_or_token(), lambda_ty.clone(), expected_ty);
-        }
-
-        lambda_ty
     }
 
     fn infer_match_expr(&mut self, match_expr: &ast::MatchExpr, _expected: Expected) -> Ty {
