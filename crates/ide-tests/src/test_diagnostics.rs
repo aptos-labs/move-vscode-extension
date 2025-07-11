f127400daf49810446ffa7aebc740c6fef7a67c9
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-07-11 22:17:46 +0300

    reorganize diagnostics tests

---
+++ crates/ide-tests/src/test_diagnostics.rs
@@ -0,0 +1,10 @@
+mod test_replace_with_compound_expr;
+mod test_replace_with_index_expr;
+mod test_replace_with_method_call;
+
+mod test_missing_fields;
+mod test_missing_value_arguments;
+
+mod test_type_checking;
+mod test_unresolved_reference;
+mod test_unused_variables;
