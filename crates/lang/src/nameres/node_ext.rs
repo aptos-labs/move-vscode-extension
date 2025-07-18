f35a05cfad8fbb8965f5776bb103ac4edc65656e
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-07-18 15:18:24 +0300

    rework module resolution helpers

--- crates/lang/src/nameres/node_ext.rs
+++ crates/lang/src/nameres/node_ext.rs
@@ -15,15 +15,10 @@ use syntax::ast::HasItems;
 use syntax::files::{InFile, InFileExt};
 
 pub trait ModuleResolutionExt {
-    fn item_entries(&self) -> Vec<ScopeEntry>;
-    fn importable_entries(&self) -> Vec<ScopeEntry>;
-    fn related_module_specs(&self, db: &dyn SourceDatabase) -> Vec<InFile<ast::ModuleSpec>>;
-    fn importable_entries_from_related(&self, db: &dyn SourceDatabase) -> Vec<ScopeEntry>;
-}
+    fn module(&self) -> InFile<&ast::Module>;
 
-impl ModuleResolutionExt for InFile<ast::Module> {
     fn item_entries(&self) -> Vec<ScopeEntry> {
-        let (file_id, module) = self.unpack_ref();
+        let (file_id, module) = self.module().unpack();
 
         let mut entries = vec![];
         for member in module.named_items(false) {
@@ -35,7 +30,7 @@ impl ModuleResolutionExt for InFile<ast::Module> {
                     continue;
                 }
             }
-            if let Some(entry) = member.in_file(self.file_id).to_entry() {
+            if let Some(entry) = member.in_file(file_id).to_entry() {
                 entries.push(entry);
             }
         }
@@ -45,21 +40,23 @@ impl ModuleResolutionExt for InFile<ast::Module> {
 
     fn importable_entries(&self) -> Vec<ScopeEntry> {
         let mut entries = self.item_entries();
-        entries.extend(self.clone().flat_map(|it| it.global_variables()).to_entries());
+        entries.extend(self.module().flat_map(|it| it.global_variables()).to_entries());
         entries
     }
 
-    /// collects `spec MODULE {}` from all spec-related (NAME.move + NAME.spec.move) file ids
-    #[tracing::instrument(level = "debug", skip_all)]
     fn related_module_specs(&self, db: &dyn SourceDatabase) -> Vec<InFile<ast::ModuleSpec>> {
+        let module = self.module();
         let related_file_ids =
-            iter::once(self.file_id).chain(db.spec_related_files(self.file_id).data(db));
+            iter::once(module.file_id).chain(db.spec_related_files(module.file_id).data(db));
         let mut module_specs = vec![];
         for spec_related_file_id in related_file_ids {
             let source_file = source_db::parse(db, spec_related_file_id.intern(db)).tree();
             for module_spec in source_file.module_specs() {
                 let module_spec = module_spec.in_file(spec_related_file_id);
-                if module_spec.module(db).is_some_and(|item| &item == self) {
+                if module_spec
+                    .module(db)
+                    .is_some_and(|item| item.as_ref() == self.module())
+                {
                     module_specs.push(module_spec);
                 }
             }
@@ -67,7 +64,6 @@ impl ModuleResolutionExt for InFile<ast::Module> {
         module_specs
     }
 
-    #[tracing::instrument(level = "debug", skip_all)]
     fn importable_entries_from_related(&self, db: &dyn SourceDatabase) -> Vec<ScopeEntry> {
         let mut entries = vec![];
         for related_module_spec in self.related_module_specs(db) {
@@ -81,24 +77,34 @@ impl ModuleResolutionExt for InFile<ast::Module> {
 }
 
 pub trait FileResolutionExt {
-    fn importable_entries(&self) -> Vec<ScopeEntry>;
-}
+    fn file(&self) -> InFile<&ast::SourceFile>;
 
-impl FileResolutionExt for InFile<ast::SourceFile> {
     fn importable_entries(&self) -> Vec<ScopeEntry> {
         let mut entries = vec![];
-        let modules = self.clone().flat_map(|it| it.all_modules().collect());
+        let modules = self.file().flat_map(|it| it.all_modules().collect());
         for module in modules {
             if let Some(module_entry) = module.clone().to_entry() {
                 entries.push(module_entry);
             }
             entries.extend(module.importable_entries());
         }
-        let module_specs = self.clone().flat_map(|it| it.module_specs().collect());
+        let module_specs = self.file().flat_map(|it| it.module_specs().collect());
         for module_spec in module_specs {
             let items = module_spec.flat_map(|it| it.importable_items());
             entries.extend(items.to_entries());
         }
         entries
+    }
+}
+
+impl ModuleResolutionExt for InFile<ast::Module> {
+    fn module(&self) -> InFile<&ast::Module> {
+        self.as_ref()
+    }
+}
+
+impl FileResolutionExt for InFile<ast::SourceFile> {
+    fn file(&self) -> InFile<&ast::SourceFile> {
+        self.as_ref()
     }
 }
