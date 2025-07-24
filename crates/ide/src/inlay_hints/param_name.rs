4076d5a15d7b54610035befd98a56986ed8b69a4
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-07-24 19:14:59 +0200

    better show/hide heuristics for inlay parameter hints (#230)
    * better show/hide heuristics for inlay parameter hints
    
    * fix tests

--- crates/ide/src/inlay_hints/param_name.rs
+++ crates/ide/src/inlay_hints/param_name.rs
@@ -6,8 +6,10 @@
 
 use crate::inlay_hints::{InlayHint, InlayHintLabel, InlayHintPosition, InlayHintsConfig, InlayKind};
 use ide_db::RootDatabase;
+use ide_db::defs::BUILTIN_RESOURCE_FUNCTIONS;
 use lang::Semantics;
-use lang::node_ext::callable::CallableItem;
+use std::collections::HashSet;
+use std::sync::LazyLock;
 use syntax::files::InFile;
 use syntax::{AstNode, ast};
 
@@ -22,32 +24,58 @@ pub(super) fn hints(
     }
 
     let callable = sema.callable(&call_expr)?;
-    if matches!(callable.callable_item, CallableItem::AssertMacro) {
+    if callable
+        .callable_item
+        .name()
+        .is_some_and(|name| KNOWN_CALLABLES.contains(name.as_str()))
+    {
         return None;
     }
 
     let params = callable.params()?;
+    // skip if only a single parameter
+    if params.len() == 1 {
+        return None;
+    }
+
     let arg_exprs = call_expr.value.arg_exprs();
     for (param_name, arg_expr) in params.into_iter().map(|it| it.name).zip(arg_exprs) {
         if let (Some(param_name), Some(arg_expr)) = (param_name, arg_expr) {
-            if matches!(arg_expr, ast::Expr::Literal(_) | ast::Expr::BinExpr(_)) {
-                let mut label = InlayHintLabel::simple(param_name, None, None);
-                if config.render_colons {
-                    label.append_str(": ");
-                }
-                acc.push(InlayHint {
-                    range: arg_expr.syntax().text_range(),
-                    kind: InlayKind::Parameter,
-                    label,
-                    text_edit: None,
-                    position: InlayHintPosition::Before,
-                    pad_left: false,
-                    pad_right: false,
-                    resolve_parent: None,
-                });
+            // leading underscore is ignored
+            let param_name = param_name.trim_start_matches("_");
+            let arg_expr = arg_expr.syntax();
+            if arg_expr
+                .text()
+                .to_string()
+                .to_lowercase()
+                .contains(&param_name.to_lowercase())
+            {
+                continue;
             }
+            let mut label = InlayHintLabel::simple(param_name, None, None);
+            if config.render_colons {
+                label.append_str(": ");
+            }
+            acc.push(InlayHint {
+                range: arg_expr.text_range(),
+                kind: InlayKind::Parameter,
+                label,
+                text_edit: None,
+                position: InlayHintPosition::Before,
+                pad_left: false,
+                pad_right: false,
+                resolve_parent: None,
+            });
         }
     }
 
     Some(())
 }
+
+static KNOWN_CALLABLES: LazyLock<HashSet<&str>> = LazyLock::new(|| {
+    let mut res = HashSet::new();
+    res.extend(BUILTIN_RESOURCE_FUNCTIONS.clone());
+    res.insert("assert!");
+    res.insert("exists");
+    res
+});
