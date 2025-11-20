f71d8e861f7fc8c67de34d2055b25ecf46735801
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-11-20 23:13:23 +0300

    add new MAX_U*, MAX_I* constants, add __COMPILE_FOR_TESTING__

--- crates/lang/src/builtins_file.rs
+++ crates/lang/src/builtins_file.rs
@@ -10,12 +10,20 @@ use vfs::{Vfs, VfsPath};
 // language=Move
 pub const BUILTINS_FILE: &str = r#"
     module 0x0::builtins {
+        const __COMPILE_FOR_TESTING__: bool = false;
+
         const MAX_U8: u8 = 255;
         const MAX_U16: u16 = 65535;
         const MAX_U32: u32 = 4294967295;
         const MAX_U64: u64 = 18446744073709551615;
         const MAX_U128: u128 = 340282366920938463463374607431768211455;
         const MAX_U256: u256 = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+        const MAX_I8: i8 = 255;
+        const MAX_I16: i16 = 65535;
+        const MAX_I32: i32 = 4294967295;
+        const MAX_I64: i64 = 18446744073709551615;
+        const MAX_I128: i128 = 340282366920938463463374607431768211455;
+        const MAX_I256: i256 = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
         /// Removes `T` from address and returns it.
         /// Aborts if address does not hold a `T`.
--- crates/lang/src/nameres/is_visible.rs
+++ crates/lang/src/nameres/is_visible.rs
@@ -135,8 +135,10 @@ pub fn is_visible_in_context(
     }
 
     let item_module = item.syntax().containing_module();
-    // 0x0::builtins module functions are always visible
-    if item.syntax().kind() == FUN && item_module.clone().is_some_and(|m| m.is_builtins()) {
+    // 0x0::builtins module functions and consts are always visible
+    if matches!(item.syntax().kind(), FUN | CONST)
+        && item_module.clone().is_some_and(|m| m.is_builtins())
+    {
         return None;
     }
 
