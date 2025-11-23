62ac9684f589d96f3164797979e582d177c962c2
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-11-23 17:44:57 +0300

    allow structs / enums to have visibility modifier

--- crates/ide-tests/src/resolve/test_resolve_1.rs
+++ crates/ide-tests/src/resolve/test_resolve_1.rs
@@ -612,3 +612,109 @@ fn test_resolve_test_only_under_test_with_addresses() {
     "#,
     )
 }
+
+#[test]
+fn test_resolve_public_struct_constructor() {
+    // language=Move
+    check_resolve(
+        r#"
+        module 0x1::s {
+            public struct S { val: u8 }
+                        //X
+        }
+        module 0x1::main {
+            use 0x1::s::S;
+            fun main() {
+                S { val: 1 };
+              //^
+            }
+        }
+    "#,
+    )
+}
+
+#[test]
+fn test_resolve_public_struct_constructor_field() {
+    // language=Move
+    check_resolve(
+        r#"
+        module 0x1::s {
+            public struct S { val: u8 }
+                             //X
+        }
+        module 0x1::main {
+            use 0x1::s::S;
+            fun main() {
+                S { val: 1 };
+                  //^
+            }
+        }
+    "#,
+    )
+}
+
+#[test]
+fn test_resolve_public_struct_dot_field() {
+    // language=Move
+    check_resolve(
+        r#"
+        module 0x1::s {
+            public struct S { val: u8 }
+                             //X
+        }
+        module 0x1::main {
+            use 0x1::s::S;
+            fun main(s: S) {
+                s.val;
+                //^
+            }
+        }
+    "#,
+    )
+}
+
+#[test]
+fn test_public_enum_match() {
+    // language=Move
+    check_resolve(
+        r#"
+        module 0x1::s {
+            public enum S { One, Two }
+                          //X
+        }
+        module 0x1::main {
+            use 0x1::s::S;
+            fun main(s: S) {
+                match (s) {
+                    S::One => 1,
+                     //^
+                    S::Two => 2,
+                };
+            }
+        }
+    "#,
+    )
+}
+
+#[test]
+fn test_public_enum_match_field() {
+    // language=Move
+    check_resolve(
+        r#"
+        module 0x1::s {
+            public enum S { One { val: u8 }, Two { val: u8 } }
+                                 //X
+        }
+        module 0x1::main {
+            use 0x1::s::S;
+            fun main(s: S) {
+                match (s) {
+                    S::One { val } => val,
+                            //^
+                    S::Two { val } => val,
+                };
+            }
+        }
+    "#,
+    )
+}
